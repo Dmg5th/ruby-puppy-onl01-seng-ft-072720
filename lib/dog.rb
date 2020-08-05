6d43318ae39a6eb9619880a9cfdd0f47b12763fd
Daniel Gould <dmgould310@gmail.com>
2020-08-05 20:41:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,11 +11,10 @@ class Dog
     @@all   
     end 
     
-    def self.print_all
-      @@all.each do |dog| 
-        puts "#{dog}" 
-      end 
-    end 
+   
+  def self.print_all
+    @@all.each { |dog| puts dog.name }  
+  end 
 
 
 end
